9455ac9ceb1f06c3690933c87d04b0340049a566
mqklin <mqklin@gmail.com>
2015-07-05 20:04:27 +0600

    add boostrap file to make es6 using possible

---
+++ app1/boostrap.js
@@ -0,0 +1,9 @@
+// bootstrap.js file
+
+var System = require('es6-module-loader').System;
+
+System.import('./index').then(function(index) {
+    index.run(__dirname);
+}).catch(function(err){
+    console.log('err', err);
+});
