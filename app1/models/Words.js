2219677e7be56dccc8fdff07aff53733fdf8d402
mqklin <mqklin@gmail.com>
2015-07-06 13:46:37 +0600

    исправлен тип Number на String

--- app1/models/Words.js
+++ app1/models/Words.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var WordsSchema = new mongoose.Schema({
   str: String,
-  weight: Number
+  weight: String
 });
 
 module.exports = mongoose.model('words', WordsSchema);
