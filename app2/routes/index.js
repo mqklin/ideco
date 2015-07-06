281cccd808311734a8af8d3fe881f7b7863d6e87
mqklin <mqklin@gmail.com>
2015-07-06 17:26:59 +0600

    работающее второе приложение

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -1,8 +1,25 @@
 var router = require('express').Router();
 var Words = require('../models/Words');
-var RAM = require('./RAM');
 var validator = require('./validator');
 
+//корень
+router.get('/', (req, res) => {
+  res.status(200);
+  res.send({
+    "links": [
+      {
+          "href": "/text",
+          "rel": "textWeight",
+          "method": "POST",
+          "require": {
+            "text": "String"
+          }
+      }
+    ]
+  });
+});
+
+var _words = [];
 //СОСЧИТАТЬ ВЕС СЛОВ
 router.post('/text', function(req, res) {
   var text = req.body.text;
@@ -10,13 +27,37 @@ router.post('/text', function(req, res) {
   var err = validator.textForm(text);
   if (err){
     res.status(400);
-    res.send(err);
+    res.send({"msg": err});
     return;
-  } 
-
-  res.status(200);
-  res.send(RAM.getTextWeight(text));
+  }
+  
+  _words = [];
+  Words.find({}, (err, words) => {
+    words.forEach(word => _words.push(word));
+    var textWords = text.split(/[^a-zA-Z]+/g);
+    if (textWords[0] === "") textWords.shift();
+    if (textWords[textWords.length - 1] === "") textWords.pop();
+    var ans = 
+      textWords.reduce(
+        (acc, word) => {
+          var _word = findWord(word);
+          if (_word === null) return acc;
+          return acc + _word.weight;
+        }, 
+        0
+      ).toString();
+    res.status(200);
+    res.send({"weight": ans});
+  });
 });
+
+function findWord(str){
+  for (var word of _words){
+    if (word.str === str) return word;
+  }
+  return null;
+}
+
 
 
 module.exports = router;
