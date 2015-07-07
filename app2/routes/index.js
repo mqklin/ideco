a2b01a93612754b699d878073051568a5df81fa2
mqklin <mqklin@gmail.com>
2015-07-07 14:24:09 +0600

    small fixes

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -1,8 +1,7 @@
 var router = require('express').Router();
 var Words = require('../models/Words');
-var validator = require('./validator');
 
-//корень
+//root
 router.get('/', (req, res) => {
   res.status(200);
   res.send({
@@ -19,50 +18,37 @@ router.get('/', (req, res) => {
   });
 });
 
-var _words = [];
-//СОСЧИТАТЬ ВЕС СЛОВ
+
+//get text weight
 router.post('/text', function(req, res) {
   var text = req.body.text;
 
-  var err = validator.textForm(text);
-  if (err){
+  if (typeof(text) === 'undefined' || text === ''){
     res.status(400);
-    res.send({"msg": err});
+    res.send({"msg": "Please, pass the text"});
     return;
   }
   
-  _words = [];
   Words.find({}, (err, words) => {
     if (err) {
       res.status(500);
       res.send({"msg": "Internal Server Error"});
       return;
     }
-    words.forEach(word => _words.push(word));
+
     var textWords = text.split(/[^a-zA-Z]+/g);
     if (textWords[0] === "") textWords.shift();
     if (textWords[textWords.length - 1] === "") textWords.pop();
-    var ans = 
-      textWords.reduce(
-        (acc, word) => {
-          var _word = findWord(word);
-          if (_word === null) return acc;
-          return acc + _word.weight;
-        }, 
-        0
-      ).toString();
+
+    var ans = textWords.reduce((acc, wordStr) => {
+          for (var word of words){
+            if (word.str === wordStr) return acc + word.weight;
+          }
+          return acc;
+    }, 0).toString();
     res.status(200);
     res.send({"weight": ans});
   });
 });
-
-function findWord(str){
-  for (var word of _words){
-    if (word.str === str) return word;
-  }
-  return null;
-}
-
-
 
 module.exports = router;
--- app2/routes/validator.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  textForm: function(text){
-    if (typeof(text) === 'undefined' || text === '')
-      return 'Please, pass the text';
-    return null;
-  }
-};
