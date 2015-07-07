9bb081fcf4440934680feb86434839d48eb6b2c4
mqklin <mqklin@gmail.com>
2015-07-07 13:19:35 +0600

    cors

--- app1/index.js
+++ app1/index.js
@@ -10,12 +10,21 @@ mongoose.connect('mongodb://localhost/ideco');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 var indexRoute = require('./routes/index');
 app.use('/', indexRoute);
+
+
 
 http.createServer(app).listen('8888');
 console.log('Listen server on localhost:8888');
